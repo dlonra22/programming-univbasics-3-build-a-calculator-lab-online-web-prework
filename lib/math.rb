6c0f6fbee7784ce645a9c35dea7277fa5ede537e
arnold bwaila <arnold.bwaila@gmail.com>
2019-07-29 20:07:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,22 +1,25 @@
 def addition(num1, num2)
-total = num1+num2
+ result = num1+num2
 puts total
 end
 
 def subtraction(num1, num2)
-result = n
+result = num1 - num2
+puts result
 end
 
 def division(num1, num2)
-
+result = num1/num2
+puts result
 end
 
 def multiplication(num1, num2)
-
+ result = num1 * num2
+ puts result
 end
 
 def modulo(num1, num2)
-
+ result = 
 end
 
 def square_root(num)
